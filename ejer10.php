2f00d1a12d0184234b18b8a41f8b70dd7cb93c57
Inigopees <iperezes18dw@ikzubirimanteo.com>
2019-09-25 16:44:37 +0200

    he acabado el ejercicio10

--- ejer10.php
+++ ejer10.php
@@ -20,7 +20,20 @@
 				}
 			}
 
-			if()
+			if($total_compra<19){
+				switch ($tipo_compra) {
+					case 'ropa':
+						echo "Los gastos de envio son 9 euros. El total te saldria ".($total_compra+9)*0.21." euros.";
+					break;
+					case 'mascotas':
+						echo "No se puede realizar el envio. El total seria, ".($total_compra)*0.10." euros.";
+					break;
+				}
+			}elseif($total_compra>19 && $total_compra<40){
+				echo "Los gastos de envio son 9 euros. El total seria ".($total_compra+9)*iva($tipo_compra)." euros.";
+			}elseif($total_compra>80){
+				echo "Los gastos de envio son gratuitos. El total seria ".($total_compra)*iva($tipo_compra)." euros.";
+			}
 
 		?>
 	</body>
